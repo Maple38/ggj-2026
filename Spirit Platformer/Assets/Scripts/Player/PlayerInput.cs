bfbf71a66fd59d1b0c1c9996d3bb8c857e389069
@Maple38 <dimness_pampas_8s@icloud.com>
2026-01-30 23:11:51 +0000

    Created a player input script

---
+++ Spirit Platformer/Assets/Scripts/Player/PlayerInput.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInput : MonoBehaviour
+{
+    private PlayerMovement _playerMovement;
+
+    private InputAction _actionMove;
+    private InputAction _actionJump;
+    
+    void Awake()
+    {
+        _playerMovement = GetComponent<PlayerMovement>();
+
+        _actionMove = InputSystem.actions.FindAction("Move");
+        _actionJump = InputSystem.actions.FindAction("Jump");
+    }
+    void Update()
+    {
+        if (_actionMove.IsPressed())
+        {
+            _playerMovement.Run(_actionMove.GetControlMagnitude());
+        }
+        else
+        {
+            _playerMovement.Decelerate();
+        }
+        
+    }
+}
